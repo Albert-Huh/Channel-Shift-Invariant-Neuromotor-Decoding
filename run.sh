008f1a9bd9a0c089c33855e914a657293e758800
Syzygianinfern0 <spsharan2000@gmail.com>
2025-12-01 12:03:30 -0600

    Update run.sh to switch CUDA device and modify training commands for the original model

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export CUDA_VISIBLE_DEVICES="2"
+# export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="3"
 
 # Download data
 # python -m generic_neuromotor_interface.scripts.download_data \
@@ -14,14 +15,22 @@ export CUDA_VISIBLE_DEVICES="2"
 #     --output-dir models/
 
 # Original Model
-# python -m generic_neuromotor_interface.train \
-#     --config-name=handwriting \
-#     data_location=$(pwd)/data \
-#     # data_module/data_split=handwriting_mini_split
-
-# SetTransformer Model
 python -m generic_neuromotor_interface.train \
     --config-name=handwriting \
-    lightning_module.network.use_set_transformer=true \
+    lightning_module.network.use_set_transformer=false \
+    trainer.max_epochs=40 \
+    +callbacks.1.save_top_k=-1 \
+    +callbacks.1.every_n_epochs=1 \
     data_location=$(pwd)/data \
-    # data_module/data_split=handwriting_mini_split
+
+# SetTransformer Model
+# python -m generic_neuromotor_interface.train \
+#     --config-name=handwriting \
+#     lightning_module.network.use_set_transformer=true \
+#     trainer.max_epochs=40 \
+#     +callbacks.1.save_top_k=-1 \
+#     +callbacks.1.every_n_epochs=1 \
+#     data_location=$(pwd)/data \
+
+# /home/ss99569/miniforge3/envs/fun/bin/python ~/bin/notify.py "handwriting settransformer model training done"                                                                                                                                                                                   
+/home/ss99569/miniforge3/envs/fun/bin/python ~/bin/notify.py "handwriting original model training done"                                                                                                                                                                                   
